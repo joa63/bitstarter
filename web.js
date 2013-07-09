0e6fa24568e1f394e97b735004d7e579f8fd4260
Jorge Ocon <jorge.ocon@gmail.com>
2013-07-09 20:27:46 +0000

    Updated web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
 
+var sfs = require('fs');
+
+var fileData = sfs.readFileSync("index.html");
+var fileString = fileData.toString();
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fileString);
 });
 
 var port = process.env.PORT || 5000;
